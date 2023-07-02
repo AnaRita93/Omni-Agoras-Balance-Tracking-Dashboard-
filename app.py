c4c92d1e282a745e5777f9414cbfd549433dc5fa
ARODS <rita@vmd62396.contaboserver.net>
2023-07-02 21:55:55 +0200

    Updated web app interface

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 import pandas as pd
 import numpy as np
 import plotly.express as px
+import plotly.graph_objects as go
 from datetime import datetime, timedelta
 import dash
 import dash_bootstrap_components as dbc
@@ -20,6 +21,7 @@ df = pd.read_csv('data/omni_extracted.csv')
 df.drop(['id'], axis=1, inplace=True)
 df['timestamp'] = pd.to_datetime(df['timestamp'])
 df['date'] = df['timestamp'].dt.date
+#df['date'] = pd.to_datetime(df['date'])
 df = df.sort_values(['address', 'date', 'timestamp'])
 df = df.drop_duplicates(subset=['address', 'date'], keep='first')
 df = df.round(3)
@@ -27,12 +29,36 @@ df_total_balance = df.groupby('date')['balance'].sum().reset_index()
 
 #Find latest balances
 latest_df = df.sort_values('date').drop_duplicates('address', keep='last')
-total_balance = latest_df['balance'].sum()
-latest_df['relative_weight'] = latest_df['balance'] / total_balance
-latest_df = latest_df[['address', 'balance', 'relative_weight','date']]
-latest_df = latest_df.round(3)
-latest_df.sort_values(by='balance', ascending=False)
 
+## Flag Balances with empty wallets 
+#majority_date = latest_df['date'].value_counts().idxmax()
+#majority_date = pd.to_datetime(latest_df['date'].mode()[0])
+#latest_df.loc[latest_df['date'] < majority_date, 'balance'] = 0
+
+majority_date = pd.to_datetime(latest_df['date'].mode()[0])
+majority_date = pd.Timestamp(majority_date)
+latest_df.loc[latest_df['date'].apply(pd.Timestamp) < majority_date, 'balance'] = 0
+
+## Calculate each address contribution  
+total_balance = latest_df['balance'].sum()
+latest_df['percentage'] = latest_df['balance'] / total_balance
+latest_df = latest_df.round(3)
+
+## Attribute Labels to addresses 
+latest_df['label']= ''
+address_labels = {
+    '14gF3Up7wdRdkxAL4GgQLdnM8CThgDUSHR': 'ICO Wallet',
+    '1DUb2YYbQA1jjaNYzVXLZ7ZioEhLXtbUru': 'Bittrex',
+    '1KGv7PL3zz5CE5jz1dtFnauMJHJLkKjXAE': 'BCEX',
+    '1vxQFvJ8k6cQzNxdo7cBwCHyNwFhRLN1M': 'Whitebit',
+    
+}
+latest_df['label'] = latest_df['address'].map(address_labels)
+
+## Final formating of latest_df 
+latest_df = latest_df[['address', 'balance', 'percentage','label','date']]
+latest_df = latest_df.sort_values(by='balance', ascending=False)
+latest_df = latest_df.reset_index(drop=True)
 
 #Calculation of balance differences 
 df_diff = df
@@ -49,15 +75,28 @@ changes_found_df = df_diff[df_diff['abs_diff'] > 0] #['address'] #.unique()
 changes_found_df = changes_found_df[['address', 'date', 'abs_diff', 'rel_diff']]
 matching_addresses = changes_found_df['address'].tolist()
 
+def create_figure(df, title):
+    fig = px.line(df, x="date", y="balance", title=title,
+    labels={"date": "Date","balance": "Balance"},
+    template='plotly_white')
+    
+    # Add markers and increase the line width
+    fig.update_traces(mode='lines+markers', line=dict(width=2)),
+    fig.update_xaxes(tickangle=45, dtick="D1", tickformat="%d-%m-%Y")
+        
+    return fig
+
 # Initialize the app
 app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
 
 app.layout = dbc.Container([
+    
 
     dbc.Row([dbc.Col(html.H1('Omni Agoras Balance Tracking',
                               className='text-center text-primary, mb-4'),
                               style={'marginTop': '20px', 'marginBottom': '20px'},
                       width=12)]),
+    dcc.Store(id='selected-address'),
 
     dbc.Row([
 
@@ -65,6 +104,9 @@ app.layout = dbc.Container([
             dbc.Card([
                 dbc.CardBody([
                     html.H5('Latest Balances', className='card-title'),
+                    
+                    dcc.Graph(id='pie-chart'),
+                    
                     dash_table.DataTable(
                         id='latest_df',
                         columns=[
@@ -73,21 +115,24 @@ app.layout = dbc.Container([
                              'format': Format(precision=3, scheme=Scheme.fixed)} if i=='balance' else 
                             {"name": i, "id": i, 'deletable': False, 'selectable': True,
                              'type': 'numeric',
-                             'format': FormatTemplate.percentage(2)} if i=='relative_weight' else 
+                             'format': FormatTemplate.percentage(2)} if i=='percentage' else 
                             {"name": i, "id": i}
                             for i in latest_df.columns],
                         data=latest_df.to_dict('records'),
                         editable=False,
                         filter_action="native",
-                        sort_action="native",
-                        sort_by=[{'column_id':'balance', 'direction':'desc'}],
-                        page_action="native",
+                        #sort_action="native",
+                        #sort_by=[{'column_id':'balance', 'direction':'desc'}],
+                        page_action="none",
                         page_current=0,
                         page_size=24,
-                        style_cell={'textAlign': 'center', 'padding': '5px'},
+                        #row_selectable="multi",  
+                        #selected_rows=[],  
+                        style_cell={'textAlign': 'center', 'padding': '5px', 'width': '50px', 'minWidth': '30px', 'maxWidth': '250px', 'overflow': 'hidden'},
                         style_cell_conditional=[{
                             'if': {'column_id': 'balance'},
                             'textAlign': 'right'}],
+                        style_data ={'width': '50px', 'minWidth': '30px', 'maxWidth': '250px', 'overflow': 'hidden'},
                         style_data_conditional=[{
                             'if': {'column_id': 'address', 
                                   'filter_query': '{{address}} = {}'.format(addr)},
@@ -98,11 +143,11 @@ app.layout = dbc.Container([
                        
                         style_as_list_view=True,
                         style_header={'backgroundColor': 'white','fontWeight': 'bold'},
-                        style_table={'height': '800px', 'overflowY': 'auto'},
+                        style_table={'height': '400px', 'overflowY': 'auto'},
                     ),
                 ]),
             ], className='mb-4'),
-        ], width=6),
+        ],width=6),
 
         dbc.Col([
             dbc.Card([
@@ -110,6 +155,7 @@ app.layout = dbc.Container([
                 dbc.CardBody([
                     
                     html.H5("Balance Changes ", className='card-title'),
+                    html.P('Please select a time frame'),
                     
                       dcc.Dropdown(
                         id='dropdown-time-filter',
@@ -149,6 +195,8 @@ app.layout = dbc.Container([
                     
                     html.H5("Balance Changes Over Time", className='card-title'),
                     
+                    html.P('Please click on an address from Latest Balances table (on the left).'),
+                    
                     html.Button('Clear Selection', id='clear-button', n_clicks=0),
                     
                     dcc.Graph(id='line-chart')])
@@ -156,7 +204,19 @@ app.layout = dbc.Container([
         ], width=6)
 
     ], justify="around", align="start")  # Center and align to the top
-])
+    
+],fluid=True, style={'max-width': '90%'})
+
+@app.callback(
+    Output('pie-chart', 'figure'),
+    Input('latest_df', 'data')
+)
+def update_pie_chart(data):
+    latest_df = pd.DataFrame(data)
+    fig = go.Figure(data=[go.Pie(labels=latest_df['address'], values=latest_df['balance'], hole=.3, hoverinfo='label+percent+value', textinfo='none')])
+    fig.update_layout(showlegend=False, title_text='Relative Weights of Addresses')
+    return fig
+
 
 @app.callback(
     Output('changes_found_df', 'data'),
@@ -166,62 +226,53 @@ def update_table(time_range):
     if time_range:
         time_values = {'1D': 1, '7D': 7, '1M': 30, '3M': 90, '6M': 180, '1Y': 365}
         num_days = time_values[time_range]
-
-        # Assuming 'date' column is a string formatted as 'YYYY-MM-DD'
         changes_found_df['date'] = pd.to_datetime(changes_found_df['date'])
         cutoff_date = datetime.now() - timedelta(days=num_days)
 
         filtered_df = changes_found_df[changes_found_df['date'] > cutoff_date]
     else:
-        # If no time range is selected, show all data
         filtered_df = changes_found_df
 
     return filtered_df.to_dict('records')
 
+@app.callback(
+    Output('selected-address', 'data'),
+    Input('latest_df', 'active_cell'),
+    Input('changes_found_df', 'active_cell'),
+    Input('clear-button', 'n_clicks'),
+    State('latest_df', 'data'),
+    State('changes_found_df', 'data'))
+def update_selected_address(active_cell_latest, active_cell_changes, n_clicks, data_latest, data_changes):
+    ctx = dash.callback_context
+    if not ctx.triggered:
+        return "Overall Balance"
+    else:
+        input_id = ctx.triggered[0]['prop_id'].split('.')[0]
+        if input_id == 'clear-button':
+            return "Overall Balance"
+        elif input_id == 'latest_df':
+            if active_cell_latest and active_cell_latest['column_id'] == 'address':
+                return data_latest[active_cell_latest['row']]['address']
+        elif input_id == 'changes_found_df':
+            if active_cell_changes and active_cell_changes['column_id'] == 'address':
+                return data_changes[active_cell_changes['row']]['address']
 
 @app.callback(
     Output('line-chart', 'figure'),
-    Input('changes_found_df', 'active_cell'),
-     Input('latest_df', 'active_cell'),
-     State('changes_found_df', 'data'),
-     State('latest_df', 'data')
+    Input('selected-address', 'data'),
 )
-def update_line_chart(active_cell_changes, active_cell_latest, data_changes, data_latest):
-    ctx = dash.callback_context
-    trigger_id = ctx.triggered[0]['prop_id'].split('.')[0]
-
-    if trigger_id == 'changes_found_df' and active_cell_changes and active_cell_changes['column_id'] == 'address':
-        selected_address = data_changes[active_cell_changes['row']]['address']
-    elif trigger_id == 'latest_df' and active_cell_latest and active_cell_latest['column_id'] == 'address':
-        selected_address = data_latest[active_cell_latest['row']]['address']
-    else:
+def update_line_chart(selected_address):
+    if selected_address == "Overall Balance":
         return create_figure(df_total_balance, 'Total Balance Over Time')
 
     df_selected = df[df['address'] == selected_address]
+    
     return create_figure(df_selected, f'Balance for Address: {selected_address}')
 
 
-def create_figure(df_selected, title):
-    df_selected['date'] = pd.to_datetime(df_selected['date'])
-    fig = px.line(df_selected, x='date', y='balance', title=title)
-    fig.update_xaxes(tickangle=45, dtick="D1", tickformat="%d-%m-%Y")
-
-    return fig
-@app.callback(
-    [Output('latest_df', 'active_cell'),
-     Output('changes_found_df', 'active_cell')],
-    [Input('clear-button', 'n_clicks')],
-)
-def clear_active_cells(n_clicks):
-    if n_clicks > 0:
-        return None, None  # reset active cells in both DataTables
-    raise PreventUpdate  # no action if button was not clicked
-
-
-
-
+            
 if __name__ == '__main__':
-    app.run_server(debug=False,host='0.0.0.0', port=8081)
+    app.run_server(debug=False, port=8081)
 
 
 
